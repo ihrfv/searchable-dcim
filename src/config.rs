87e5e4839e1bf5461aef961d7d96acc557638eb0
Ihor Filimonov <254675014+ihrfv@users.noreply.github.com>
2026-04-02 18:07:24 +0200

    Add validation that the provided dcim_folder points indeed to a folder

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,16 @@
+use std::path::Path;
+
 pub struct Config {
     dcim_path: String,
 }
 
 impl Config {
+    /// Builds a valid Config from the env args
+    ///
+    /// # Errors
+    ///
+    /// 1. If the are not enough provided args
+    /// 2. If the first provided arg doesn't point to a directory
     pub fn build() -> Result<Config, &'static str> {
         let mut args = std::env::args();
         args.next();
@@ -11,6 +19,10 @@ impl Config {
             Some(path) => path,
             None => return Err("Didn't get a dcim folder path"),
         };
+
+        if !Path::new(&dcim_path).is_dir() {
+            return Err("The provided path is not a directory");
+        }
 
         Ok(Config { dcim_path })
     }
